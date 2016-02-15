d4b7b2adf6da6942c413807fa2aeb69f4d2b4cce
Paul Betts <paul@paulbetts.org>
2016-02-14 20:50:19 -0800

    Initial Import

---
+++ src/Squirrel.Windows.Tools/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Squirrel.Windows.Tools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Bind(ViewModel, x => x.ReleaseLocation, x => x.ReleaseLocation.Text);
+            this.OneWayBind(ViewModel, x => x.ReleaseLocation, x => x.ReleaseLocation.Text);
+            this.OneWayBind(ViewModel, x => x.ReleasesListHint, x => x.ReleasesListHint.Content);
+
+            this.WhenAny(x => x.ViewModel.ReleasesListHint, x => !String.IsNullOrWhiteSpace(x.Value))
+                .BindTo(this, x => x.ReleasesListHint.Visibility);
+
+            this.BindCommand(ViewModel, x => x.DoIt, x => x.DoIt);
+
+            ViewModel = new MainWindowViewModel();
+
+            MessageBox.Show(ViewModel.DoIt.CanExecute(null).ToString());
+        }
+
+        public MainWindowViewModel ViewModel {
+            get { return (MainWindowViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainWindowViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (MainWindowViewModel)value; }
+        }
+    }
+}
---
+++ src/Squirrel.Windows.Tools/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Squirrel.Windows.Tools
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public ReactiveCommand<List<ReleaseEntry>> CheckRemoteUpdateInfo { get; private set; }
+
+        public ReactiveCommand<Unit> DoIt { get; private set; }
+
+        ObservableAsPropertyHelper<List<ReleaseEntry>> updateInfo;
+        public List<ReleaseEntry> UpdateInfo {
+            get { return updateInfo.Value; }
+        }
+
+        string releaseLocation;
+        public string ReleaseLocation {
+            get { return releaseLocation; }
+            set { this.RaiseAndSetIfChanged(ref releaseLocation, value); }
+        }
+
+        string releasesListHint;
+        public string ReleasesListHint {
+            get { return releasesListHint; }
+            set { this.RaiseAndSetIfChanged(ref releasesListHint, value); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            ReleasesListHint = "Type in a release location URL or path to files";
+
+            CheckRemoteUpdateInfo = ReactiveCommand.CreateAsyncTask(
+                this.WhenAny(x => x.ReleaseLocation, x => !String.IsNullOrWhiteSpace(x.Value)),
+                async _ => {
+                    ReleasesListHint = "";
+
+                    var releaseData = default(String);
+                    if (Regex.IsMatch(ReleaseLocation, "^https?://", RegexOptions.IgnoreCase)) {
+                        var wc = new WebClient();
+                        releaseData = await wc.DownloadStringTaskAsync(ReleaseLocation + "/RELEASES");
+                    } else {
+                        releaseData = File.ReadAllText(releaseData, Encoding.UTF8);
+                    }
+
+                    var ret = releaseData.Split('\n')
+                        .Select(x => ReleaseEntry.ParseReleaseEntry(x))
+                        .ToList();
+
+                    return ret;
+                });
+
+            CheckRemoteUpdateInfo.ThrownExceptions
+                .Subscribe(ex => ReleasesListHint = "Failed to check for updates: " + ex.Message);
+
+            CheckRemoteUpdateInfo.ToProperty(this, x => x.UpdateInfo, out updateInfo);
+
+            this.WhenAnyValue(x => x.ReleaseLocation)
+                .Throttle(TimeSpan.FromMilliseconds(750), RxApp.MainThreadScheduler)
+                .InvokeCommand(CheckRemoteUpdateInfo);
+
+            DoIt = ReactiveCommand.CreateAsyncTask(
+                this.WhenAny(x => x.UpdateInfo, x => x.Value != null && x.Value.Count > 0),
+                async _ => { });
+        }
+    }
+}
